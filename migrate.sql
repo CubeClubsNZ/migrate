d952e64f21cfcf963e2cb70993de4e8a0eee8ad5
Tim Xie <pdt.xie@gmail.com>
2023-08-11 21:32:12 +1200

    add club organiser

--- migrate.sql
+++ migrate.sql
@@ -3,3 +3,5 @@ SELECT setval(pg_get_serial_sequence('"Meetup"', 'id'), coalesce(max(id)+1, 1), 
 SELECT setval(pg_get_serial_sequence('"Club"', 'id'), coalesce(max(id)+1, 1), false) FROM "Club";
 SELECT setval(pg_get_serial_sequence('"Result"', 'id'), coalesce(max(id)+1, 1), false) FROM "Result";
 SELECT setval(pg_get_serial_sequence('"Round"', 'id'), coalesce(max(id)+1, 1), false) FROM "Round";
+
+UPDATE "User" SET "isClubOrganiser"=true WHERE id IN (1,27,5,34,55,50,92,147,268,287,591);
