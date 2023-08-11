0561eef05425470654dd74e8c58b39ef9be9b072
Tim Xie <pdt.xie@gmail.com>
2023-08-11 23:45:51 +1200

    fix names

--- migrate.sql
+++ migrate.sql
@@ -5,3 +5,19 @@ SELECT setval(pg_get_serial_sequence('"Result"', 'id'), coalesce(max(id)+1, 1), 
 SELECT setval(pg_get_serial_sequence('"Round"', 'id'), coalesce(max(id)+1, 1), false) FROM "Round";
 
 UPDATE "User" SET "isClubOrganiser"=true WHERE id IN (1,27,5,34,55,50,92,147,268,287,591);
+
+DELETE FROM "User" WHERE id IN (530, 563, 596, 419, 574);
+
+UPDATE "User" 
+    SET name = CASE id
+                WHEN 619 THEN 'Raymond Du'
+                WHEN 260 THEN 'Rachel Gu'
+                WHEN 313 THEN 'Paratene Te Akonga Mohi'
+                WHEN 472 THEN 'James Jameson'
+                WHEN 546 THEN 'Alika Feaver'
+                WHEN 459 THEN 'Bertie'
+                WHEN 562 THEN 'Blake Webber'
+                WHEN 456 THEN 'Matthew'
+                WHEN 455 THEN 'Jim'
+                END
+    WHERE id IN (619, 260, 313, 472);
